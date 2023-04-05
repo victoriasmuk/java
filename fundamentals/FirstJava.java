308bfe26e71ca43b1ae0589d62c2ff6c4062d3b3
victoriasmuk <vicsmuk@icloud.com>
2023-04-05 13:33:36 -0400

    first java

---
+++ fundamentals/FirstJava.java
@@ -0,0 +1,7 @@
+public class FirstJava {
+    public static void main(String[] args){
+        System.out.println("My name is Victoria Smuk");
+        System.out.println("I am 23 years old");
+        System.out.println("My hometown is Hartville, Ohio");
+    }
+}
