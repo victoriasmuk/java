bb975954b0b6bf9aec2a1ace0624365bec87ee78
victoriasmuk <vicsmuk@icloud.com>
2023-04-06 13:41:24 -0400

    alfred bot complete

---
+++ fundamentals/alfredBot/AlfredQuotes.java
@@ -0,0 +1,35 @@
+// import java.util.Date;
+import java.util.Date;
+public class AlfredQuotes {
+    
+    public String basicGreeting() {
+        // You do not need to code here, this is an example method
+        return "Hello, lovely to see you. How are you?";
+    }
+    
+    public String guestGreeting(String name) {
+        // YOUR CODE HERE
+        return String.format("Hello, %s. Lovely to see you.", name);
+    }
+    
+    public String dateAnnouncement() {
+        // YOUR CODE HERE
+        return String.format("It is currently %s.", new Date());
+    }
+    
+    public String respondBeforeAlexis(String conversation) {
+        // YOUR CODE HERE
+        if(conversation.indexOf("Alexis") > -1) {
+            return "Right away, sir. She certainly isn't sophisticated enough for that.";
+        }
+        if(conversation.indexOf("Alfred") > -1){
+            return "At your service. As you wish, naturally.";
+        }
+        return "Right. And with that I shall retire.";
+    }
+    
+	// NINJA BONUS
+	// See the specs to overload the guestGreeting method
+        // SENSEI BONUS
+        // Write your own AlfredQuote method using any of the String methods you have learned!
+}
