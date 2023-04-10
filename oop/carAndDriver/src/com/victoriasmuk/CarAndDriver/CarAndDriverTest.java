90ec8d66df37133653350a2aa1e1cb74f6a93e69
victoriasmuk <vicsmuk@icloud.com>
2023-04-10 09:00:21 -0400

    updated

---
+++ oop/carAndDriver/src/com/victoriasmuk/CarAndDriver/CarAndDriverTest.java
@@ -0,0 +1,17 @@
+package com.victoriasmuk.CarAndDriver;
+
+public class CarAndDriverTest {
+
+	public static void main(String[] args) {
+		Driver Driver = new Driver();
+		
+		Driver.drive();
+		Driver.drive();
+		Driver.drive();
+		Driver.drive();
+		Driver.useBooster();
+		Driver.refuel();
+		Driver.refuel();
+		Driver.refuel();
+	}
+}
